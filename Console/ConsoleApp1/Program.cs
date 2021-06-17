27ae17911372e7fd63988760fa25ebc8068b97a8
Kevin <Kevin.meng@ksatsw.com>
2021-06-17 10:42:41 +0800

    QrCode

---
+++ Console/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] citys = new string[3];
+            citys[0] = "BeiJing";
+            citys[1] = "ShangHai";
+            citys[2] = "TianJin";
+            foreach(string item in citys)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+
+}
